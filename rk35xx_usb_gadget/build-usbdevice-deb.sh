b8f48358da311fcf945cb7eafaf0576fbe593154
pillar1989 <zuobaozhu@gmail.com>
2026-05-06 12:38:43 +0800

    usbdevice-gadget: rename package from rk3588 to rk
    The USB gadget script uses generic Rockchip configfs which works on
    both RK3576 and RK3588. Remove the misleading rk3588 suffix.

--- rk35xx_usb_gadget/build-usbdevice-deb.sh
+++ rk35xx_usb_gadget/build-usbdevice-deb.sh
@@ -4,6 +4,6 @@ PKGROOT="$(cd "$(dirname "$0")" && pwd)/usbdevice-gadget-deb"
 OUTDIR="$(cd "$(dirname "$0")" && pwd)"
 VERSION=$(awk -F': ' '/^Version:/{print $2}' "$PKGROOT/DEBIAN/control")
 ARCH=$(awk -F': ' '/^Architecture:/{print $2}' "$PKGROOT/DEBIAN/control")
-OUT="$OUTDIR/usbdevice-gadget-rk3588_${VERSION}_${ARCH}.deb"
+OUT="$OUTDIR/usbdevice-gadget-rk_${VERSION}_${ARCH}.deb"
 dpkg-deb -b "$PKGROOT" "$OUT"
 echo "$OUT"
